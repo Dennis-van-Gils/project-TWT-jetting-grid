0f77179dcdeec6c5f9a9b334f696d98d9ccce2ad
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2022-07-26 17:09:18 +0200

    Reading out all four R_Clicks collectively on a single timer

--- source_mcu/src/main.cpp
+++ source_mcu/src/main.cpp
@@ -32,32 +32,19 @@ char buf[BUFLEN]{'\0'};
 Adafruit_NeoPixel strip(LED_COUNT, PIN_LED_MATRIX, NEO_GRB + NEO_KHZ800);
 
 /*------------------------------------------------------------------------------
-  MIKROE 4-20 mA R click boards for reading out the OMEGA pressure sensors
-------------------------------------------------------------------------------*/
-
-R_Click R_click_1(PIN_R_CLICK_1, R_CLICK_1_CALIB, DAQ_DT, DAQ_LP);
-R_Click R_click_2(PIN_R_CLICK_2, R_CLICK_2_CALIB, DAQ_DT, DAQ_LP);
-R_Click R_click_3(PIN_R_CLICK_3, R_CLICK_3_CALIB, DAQ_DT, DAQ_LP);
-R_Click R_click_4(PIN_R_CLICK_4, R_CLICK_4_CALIB, DAQ_DT, DAQ_LP);
-
-/*------------------------------------------------------------------------------
   Holds actuator states and sensor readings
 ------------------------------------------------------------------------------*/
 
 struct State {
   // OMEGA pressure sensors
-  float pres_1_bitval = NAN; // [0-4095]
-  float pres_2_bitval = NAN; // [0-4095]
-  float pres_3_bitval = NAN; // [0-4095]
-  float pres_4_bitval = NAN; // [0-4095]
-  float pres_1_mA = NAN;     // [mA]
-  float pres_2_mA = NAN;     // [mA]
-  float pres_3_mA = NAN;     // [mA]
-  float pres_4_mA = NAN;     // [mA]
-  float pres_1_bar = NAN;    // [bar]
-  float pres_2_bar = NAN;    // [bar]
-  float pres_3_bar = NAN;    // [bar]
-  float pres_4_bar = NAN;    // [bar]
+  float pres_1_mA = NAN;  // [mA]
+  float pres_2_mA = NAN;  // [mA]
+  float pres_3_mA = NAN;  // [mA]
+  float pres_4_mA = NAN;  // [mA]
+  float pres_1_bar = NAN; // [bar]
+  float pres_2_bar = NAN; // [bar]
+  float pres_3_bar = NAN; // [bar]
+  float pres_4_bar = NAN; // [bar]
 };
 State state;
 
@@ -70,6 +57,54 @@ bool neo_flash = false;
 uint32_t t_neo_flash = 0;
 Adafruit_NeoPixel neo(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 */
+
+/*------------------------------------------------------------------------------
+  MIKROE 4-20 mA R click boards for reading out the OMEGA pressure sensors
+------------------------------------------------------------------------------*/
+
+R_Click R_click_1(PIN_R_CLICK_1, R_CLICK_1_CALIB);
+R_Click R_click_2(PIN_R_CLICK_2, R_CLICK_2_CALIB);
+R_Click R_click_3(PIN_R_CLICK_3, R_CLICK_3_CALIB);
+R_Click R_click_4(PIN_R_CLICK_4, R_CLICK_4_CALIB);
+
+uint32_t EMA_tick = micros();
+uint32_t EMA_obtained_interval;
+bool EMA_at_startup = true;
+float EMA_1_bitval;
+float EMA_2_bitval;
+float EMA_3_bitval;
+float EMA_4_bitval;
+
+bool R_click_poll_EMA_collectively() {
+  uint32_t now = micros();
+  float alpha; // Derived smoothing factor of the exponential moving average
+
+  if ((now - EMA_tick) >= DAQ_DT) {
+    // Enough time has passed -> Acquire a new reading.
+    // Calculate the smoothing factor every time because an exact interval time
+    // is not garantueed.
+    EMA_obtained_interval = now - EMA_tick;
+    alpha = 1.f - exp(-float(EMA_obtained_interval) * DAQ_LP * 1e-6);
+
+    if (EMA_at_startup) {
+      EMA_at_startup = false;
+      EMA_1_bitval = R_click_1.read_bitval();
+      EMA_2_bitval = R_click_2.read_bitval();
+      EMA_3_bitval = R_click_3.read_bitval();
+      EMA_4_bitval = R_click_4.read_bitval();
+    } else {
+      EMA_1_bitval += alpha * (R_click_1.read_bitval() - EMA_1_bitval);
+      EMA_2_bitval += alpha * (R_click_2.read_bitval() - EMA_2_bitval);
+      EMA_3_bitval += alpha * (R_click_3.read_bitval() - EMA_3_bitval);
+      EMA_4_bitval += alpha * (R_click_4.read_bitval() - EMA_4_bitval);
+    }
+    EMA_tick = now;
+    return true;
+
+  } else {
+    return false;
+  }
+}
 
 // -----------------------------------------------------------------------------
 //  setup
@@ -133,28 +168,21 @@ void loop() {
   //   Update R click readings
   // ---------------------------------------------------------------------------
 
-  if (R_click_1.poll_EMA()) {
+  if (R_click_poll_EMA_collectively()) {
     // DEBUG: Alarm when obtained DT interval is too large
-    if (R_click_1.get_EMA_obtained_interval() > 3000) {
+    if (EMA_obtained_interval > 3000) {
       Serial.print("Warning. Large EMA DT: ");
-      Serial.println(R_click_1.get_EMA_obtained_interval());
+      Serial.println(EMA_obtained_interval);
     }
   }
-  R_click_2.poll_EMA();
-  R_click_3.poll_EMA();
-  R_click_4.poll_EMA();
 
   if (now - tick > 1000) {
     tick = now;
 
-    state.pres_1_bitval = R_click_1.get_EMA_bitval();
-    state.pres_2_bitval = R_click_2.get_EMA_bitval();
-    state.pres_3_bitval = R_click_3.get_EMA_bitval();
-    state.pres_4_bitval = R_click_4.get_EMA_bitval();
-    state.pres_1_mA = R_click_1.get_EMA_mA();
-    state.pres_2_mA = R_click_2.get_EMA_mA();
-    state.pres_3_mA = R_click_3.get_EMA_mA();
-    state.pres_4_mA = R_click_4.get_EMA_mA();
+    state.pres_1_mA = R_click_1.bitval2mA(EMA_1_bitval);
+    state.pres_2_mA = R_click_2.bitval2mA(EMA_2_bitval);
+    state.pres_3_mA = R_click_3.bitval2mA(EMA_3_bitval);
+    state.pres_4_mA = R_click_4.bitval2mA(EMA_4_bitval);
     state.pres_1_bar = mA2bar(state.pres_1_mA, OMEGA_1_CALIB);
     state.pres_2_bar = mA2bar(state.pres_2_mA, OMEGA_2_CALIB);
     state.pres_3_bar = mA2bar(state.pres_3_mA, OMEGA_3_CALIB);
@@ -162,13 +190,8 @@ void loop() {
 
     // clang-format off
     snprintf(buf, BUFLEN,
-             "%.1f\t%.1f\t%.1f\t%.1f\t\t"
              "%.2f\t%.2f\t%.2f\t%.2f\t\t"
              "%.3f\t%.3f\t%.3f\t%.3f\n",
-             state.pres_1_bitval,
-             state.pres_2_bitval,
-             state.pres_3_bitval,
-             state.pres_4_bitval,
              state.pres_1_mA,
              state.pres_2_mA,
              state.pres_3_mA,
