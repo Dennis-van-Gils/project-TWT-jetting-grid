8ed94268c5695972f3a2ed1935fdb36c1d096e7d
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2022-09-12 18:26:41 +0200

    In progress: capturing functions into numba `jit`s

--- source_python/simplex_noise/anim_2D_tune_transparency.py
+++ source_python/simplex_noise/anim_2D_tune_transparency.py
@@ -4,99 +4,211 @@
 """
 conda create -n simplex python=3.9
 conda activate simplex
-pip install ipython numpy numba matplotlib opensimplex pylint black
+pip install ipython numpy numba matplotlib opensimplex==0.4.3
 
 # Additionally
-pip install dvg-devices
+pip install pylint black dvg-devices
 """
-
-# NOTE: Memory consumption is gigantic for 4-D Simplex noise to generate as a
-# continuous matrix. Hence, we'll have to iterate the points on request.
 
 from time import perf_counter
 
+import numpy as np
+from numba import jit, njit, prange
 from matplotlib import pyplot as plt
 from matplotlib import animation
-import numpy as np
-from numba import njit, prange
-from opensimplex.internals import _init, _noise4
 
-# opensimplex.seed(1)
-perm, foo = _init(1)
-N_pixels = 1000  # Number of pixels on a single axis
-N_frames = 600  # Number of time frames
-t_radius = 9  # Temporal radius of the time loop
+from opensimplex.internals import _init as opensimplex_init
+from opensimplex.internals import _noise4 as opensimplex_noise4
+
+# ------------------------------------------------------------------------------
+#  generate_Simplex2D_closed_timeloop
+# ------------------------------------------------------------------------------
 
 
-@njit(cache=True, parallel=True)
-def generate_noise(
-    N_frames_: int,
-    N_pixels_: int,
-    t_radius_: float,
-    x_table_: np.ndarray,
-    perm_: np.ndarray,
-):
-    noise = np.empty((1, N_frames, N_pixels, N_pixels), dtype=np.double)
-    t_factor = 2 * np.pi / N_frames_
-    for t_i in prange(N_frames_):  # pylint: disable=not-an-iterable
+@njit(
+    "float64[:, :, :](int64, int64, float64, float64, int64[:])",
+    cache=True,
+    parallel=True,
+)
+def _generate_Simplex2D_closed_timeloop(
+    N_frames: int,
+    N_pixels: int,
+    t_step: float,
+    x_step: float,
+    perm: np.ndarray,
+) -> np.ndarray:
+    t_radius = N_frames * t_step / (2 * np.pi)  # Temporal radius of the loop
+    t_factor = 2 * np.pi / N_frames
+
+    noise = np.empty((N_frames, N_pixels, N_pixels), dtype=np.double)
+    for t_i in prange(N_frames):  # pylint: disable=not-an-iterable
         t = t_i * t_factor
-        t_cos = t_radius_ * np.cos(t)
-        t_sin = t_radius_ * np.sin(t)
-        for y_i in prange(N_pixels_):  # pylint: disable=not-an-iterable
-            for x_i in prange(N_pixels_):  # pylint: disable=not-an-iterable
-                noise[0, t_i, y_i, x_i] = _noise4(
-                    x_table_[x_i], x_table_[y_i], t_sin, t_cos, perm_
+        t_cos = t_radius * np.cos(t)
+        t_sin = t_radius * np.sin(t)
+        for y_i in prange(N_pixels):  # pylint: disable=not-an-iterable
+            y = y_i * x_step
+            for x_i in prange(N_pixels):  # pylint: disable=not-an-iterable
+                x = x_i * x_step
+                noise[t_i, y_i, x_i] = opensimplex_noise4(
+                    x, y, t_sin, t_cos, perm
                 )
 
-    return noise[0]  # Ditches the dummy dimension
+    return noise
 
 
-print("Generating noise...", end="")
-t0 = perf_counter()
+def generate_Simplex2D_closed_timeloop(
+    N_frames: int = 200,
+    N_pixels: int = 1000,
+    t_step: float = 0.1,
+    x_step: float = 0.01,
+    seed: int = 1,
+) -> np.ndarray:
+    """Generates Simplex noise as 2D bitmap images that animate over time in a
+    closed-loop fashion. I.e., the bitmap image of the last time frame will
+    smoothly animate into the bitmap image of the first time frame again. The
+    animation path is /not/ a simple reversal of time in order to have the loop
+    closed, but rather is a fully unique path from start to finish.
+
+    It does so by calculating Simplex noise in 4 dimensions. The latter two
+    dimensions are used to describe a 'circle' in time, in turn used to
+    projection map the first two dimensions into bitmap images.
+
+    Args:
+        N_frames (int):
+            Number of time frames
+
+        N_pixels (int):
+            Number of pixels on a single axis
+
+        t_step (float):
+            Time step in arb. units. Good values are between 0.02 and 0.2
+
+        x_step (float):
+            Spatial step in arb. units. Good values are around 0.01
+
+        seed (int):
+            Seed value of the OpenSimplex noise
+
+    Returns:
+        The image stack as 3D matrix [time, y-pixel, x-pixel] containing the
+        Simplex noise values as a 'grayscale' intensity in floating point.
+        The output intensity is garantueed to be in the range [-1, 1], but the
+        exact extrema cannot be known a-priori and are most probably way smaller
+        than [-1, 1].
+    """
+
+    print("Generating noise...", end="")
+    tick = perf_counter()
+
+    perm, _ = opensimplex_init(seed)  # The OpenSimplex seed table
+    out = _generate_Simplex2D_closed_timeloop(
+        N_frames, N_pixels, t_step, x_step, perm
+    )
+
+    print(f" done in {(perf_counter() - tick):.2f} s")
+    return out
+
+
+# ------------------------------------------------------------------------------
+#  rescale
+# ------------------------------------------------------------------------------
+
+
+def rescale(arr: np.ndarray, symmetrically: bool = True):
+    """Rescale noise to [0, 1]
+    NOTE: In-place operation on argument `arr`
+    """
+    # NOTE: Can't seem to get @jit or @njit to work. Fails on `out` parameter of
+    # ufuncs `np.divide()` and `np.add()`. Also, `prange` will not go parallel.
+
+    print("Rescaling noise... ", end="")
+    tick = perf_counter()
+
+    in_min = np.min(arr)
+    in_max = np.max(arr)
+
+    if symmetrically:
+        f_norm = max([abs(in_min), abs(in_max)]) * 2
+        for i in prange(arr.shape[0]):  # pylint: disable=not-an-iterable
+            np.divide(arr[i], f_norm, out=arr[i])
+            np.add(arr[i], 0.5, out=arr[i])
+    else:
+        f_norm = in_max - in_min
+        for i in prange(arr.shape[0]):  # pylint: disable=not-an-iterable
+            np.subtract(arr[i], in_min, out=arr[i])
+            np.divide(arr[i], f_norm, out=arr[i])
+
+    out_min = np.min(arr)
+    out_max = np.max(arr)
+
+    print(f" done in {(perf_counter() - tick):.2f} s")
+    print(
+        f"  from [{in_min:.3f}, {in_max:.3f}] to [{out_min:.3f}, {out_max:.3f}]"
+    )
+
+
+# @njit(
+#    cache=True,
+#    parallel=True,
+# )
+def binary_map(
+    arr: np.ndarray, BW_threshold: float = 0.5, tune_transparency: float = 0.5
+):
+    arr_BW = np.zeros(arr.shape)  # , dtype=bool)
+    transp = np.zeros(arr.shape[0])  # Transparency
+
+    for i in prange(N_FRAMES):  # pylint: disable=not-an-iterable
+        # Tune transparency
+        """
+        error = 1
+        wanted_alpha = 0.3
+        # threshold = 0.5
+        threshold = 1 - wanted_alpha
+        print("---> Frame %d" % i)
+        while abs(error) > 0.02:
+            white_pxs = np.where(img_stack[i] > threshold)
+            alpha[i] = len(white_pxs[0]) / N_PIXELS / N_PIXELS
+            error = alpha[i] - wanted_alpha
+            print(error)
+            if error > 0:
+                threshold = threshold + 0.005
+            else:
+                threshold = threshold - 0.005
+        """
+
+        # Calculate transparency
+        white_pxs = np.where(arr[i] > 0.5)
+        transp[i] = len(white_pxs[0]) / arr.shape[1] / arr.shape[2]
+
+        # Binary map
+        arr_BW[i][white_pxs] = 1
+
+    return arr_BW, transp
+
+
+# ------------------------------------------------------------------------------
+#  Main
+# ------------------------------------------------------------------------------
+
+N_PIXELS = 1000  # Number of pixels on a single axis
+N_FRAMES = 200  # Number of time frames
 
 # Generate noise
-x_table = np.linspace(0, 10, N_pixels, endpoint=False)  # Spatial axis
-img_stack = generate_noise(N_frames, N_pixels, t_radius, x_table, perm)
-img_stack_min = np.min(img_stack)
-img_stack_max = np.max(img_stack)
+img_stack = generate_Simplex2D_closed_timeloop(
+    N_pixels=N_PIXELS,
+    N_frames=N_FRAMES,
+    t_step=0.1,
+    x_step=0.01,
+)
 
-elapsed = perf_counter() - t0
-print(f" done in {elapsed:.2f} s")
-print(f"  min = {img_stack_min:.3f}")
-print(f"  max = {img_stack_max:.3f}")
-print("Rescaling noise... ", end="")
+# Rescale noise
+rescale(img_stack, symmetrically=True)
+
+print("More processing... ", end="")
 t0 = perf_counter()
 
-# Rescale noise symmetrically to [0, 1]
-img_stack_BW = np.zeros((N_frames, N_pixels, N_pixels), dtype=bool)
-alpha = np.zeros(N_frames)  # Grid transparency
-f_norm = max([abs(img_stack_min), abs(img_stack_max)]) * 2
-for i in prange(N_frames):  # pylint: disable=not-an-iterable
-    np.divide(img_stack[i], f_norm, out=img_stack[i])
-    np.add(img_stack[i], 0.5, out=img_stack[i])
+img_stack_BW, alpha = binary_map(img_stack)
 
-    # And tune transparency
-    d_alpha = 1
-    wanted_alpha = 0.3
-    # threshold = 0.5
-    threshold = 1 - wanted_alpha
-    print("---> Frame %d" % i)
-    while abs(d_alpha) > 0.02:
-        white_pxs = np.where(img_stack[i] > threshold)
-        alpha[i] = len(white_pxs[0]) / N_pixels / N_pixels
-        d_alpha = alpha[i] - wanted_alpha
-        # print(d_alpha)
-        if d_alpha > 0:
-            threshold = threshold + 0.002
-        else:
-            threshold = threshold - 0.002
-
-    # Calculate grid transparency
-    # white_pxs = np.where(img_stack[i] > 0.5)
-    # alpha[i] = len(white_pxs[0]) / N_pixels / N_pixels
-
-    # Binary map
-    img_stack_BW[i][white_pxs] = 1
 
 img_stack_min = np.min(img_stack)
 img_stack_max = np.max(img_stack)
@@ -129,7 +241,7 @@ def init_anim():
 
 
 def anim(j):
-    img.set_data(img_stack_BW[j])
+    img.set_data(img_stack[j])
     frame_text.set_text(f"frame {j:03d}, transparency = {alpha[j]:.2f}")
     return img, frame_text
 
@@ -137,18 +249,18 @@ def anim(j):
 ani = animation.FuncAnimation(
     fig,
     anim,
-    frames=N_frames,
+    frames=N_FRAMES,
     interval=40,
     init_func=init_anim,  # blit=True,
 )
 
-plt.grid(False)
-plt.axis("off")
+# plt.grid(False)
+# plt.axis("off")
 plt.show()
 
 plt.figure(2)
 plt.plot(alpha)
-plt.xlim(0, N_frames)
+plt.xlim(0, N_FRAMES)
 plt.title("transparency")
 plt.xlabel("frame #")
 plt.ylabel("alpha [0 - 1]")
