d97afe8c7147381993e8346c45da88a19d597c96
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2022-11-28 13:42:26 +0100

    Added "pump enabled" LED indicator

--- src_mcu_pump_safety/src/main.cpp
+++ src_mcu_pump_safety/src/main.cpp
@@ -2,7 +2,7 @@
  * @file    Main.cpp
  * @author  Dennis van Gils (vangils.dennis@gmail.com)
  * @version https://github.com/Dennis-van-Gils/project-TWT-jetting-grid
- * @date    21-11-2022
+ * @date    28-11-2022
  *
  * @brief   Firmware for the pump safety microcontroller.
  *
@@ -16,24 +16,27 @@
 There are two microcontroller (MCU) boards used in the TWT jetting grid. The
 main MCU (Adafruit M4 Feather Express) is responsible for driving the solenoid
 valves and LED matrix and communicates via USB to the Python main program
-running on a PC. The second MCU (Adafruit M0 Feather Express) acts as a safety
-controller, governing the relay that allows turning the jetting pump on and off
-via terminal X1 of its frequency inverter.
+running on a PC. The second MCU (Adafruit Feather M0 Basic Proto) acts as a
+safety controller, governing the relay that allows turning the jetting pump on
+and off via terminal X1 of its frequency inverter.
 
-The main MCU should send a digital 'safety' pulse once every N ms over to the
-safety MCU as indication that the main MCU is still operating all right. As long
-as the safety MCU receives pulses within the set time period, the 'pump on'
-relay will be engaged.
+The main MCU should send a digital 'safety' pulse at least once every
+`SAFETY_PULSE_TIMEOUT` ms over to the safety MCU as indication that the main MCU
+is still operating all right. As long as the safety MCU receives pulses within
+the set time period, the 'pump on' relay will be engaged.
 */
 
 #include "Adafruit_SleepyDog.h"
 #include <Arduino.h>
 
 const uint8_t PIN_PUMP_RELAY = 5;
+const uint8_t PIN_PUMP_FRONT_PANEL_LED = 10;
 const uint8_t PIN_SAFETY_PULSE_IN = A0;
-
 const uint16_t SAFETY_PULSE_TIMEOUT = 100; // [ms]
-const uint16_t WATCHDOG_TIMEOUT = 200;     // [ms]
+
+// The microcontroller will auto-reboot when it fails to get a
+// `Watchdog.reset()` within this time period [ms]
+const uint16_t WATCHDOG_TIMEOUT = 200; // [ms]
 
 volatile bool received_pulse = false;
 void my_isr() { received_pulse = true; }
@@ -43,12 +46,19 @@ void my_isr() { received_pulse = true; }
 ------------------------------------------------------------------------------*/
 
 void setup() {
+  // Pump relay
   pinMode(PIN_PUMP_RELAY, OUTPUT);
   digitalWrite(PIN_PUMP_RELAY, LOW);
 
+  // Front panel LED indicating pump relay status
+  pinMode(PIN_PUMP_FRONT_PANEL_LED, OUTPUT);
+  digitalWrite(PIN_PUMP_FRONT_PANEL_LED, LOW);
+
+  // Onboard LED always on
   pinMode(PIN_LED, OUTPUT);
   digitalWrite(PIN_LED, HIGH);
 
+  // Safety pulses coming from the main MCU
   pinMode(PIN_SAFETY_PULSE_IN, INPUT_PULLDOWN);
   attachInterrupt(digitalPinToInterrupt(PIN_SAFETY_PULSE_IN), my_isr, RISING);
 
@@ -80,6 +90,7 @@ void loop() {
 
   if (prev_state_relay != engage_relay) {
     digitalWrite(PIN_PUMP_RELAY, engage_relay);
+    digitalWrite(PIN_PUMP_FRONT_PANEL_LED, engage_relay);
     prev_state_relay = engage_relay;
   }
 
