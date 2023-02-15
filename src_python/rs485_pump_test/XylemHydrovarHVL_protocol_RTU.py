cd795845d8d6ed43fac3b02b4672c66412b454a9
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2023-02-15 15:53:36 +0100

    Framework in progress

--- src_python/rs485_pump_test/XylemHydrovarHVL_protocol_RTU.py
+++ src_python/rs485_pump_test/XylemHydrovarHVL_protocol_RTU.py
@@ -1,40 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Notes on cryptic manual "HVL 2.015 - 4.220 Modbus Protocol & Parameters"
-------------------------------------------------------------------------
+"""Modbus RTU protocol (RS485) function library for a Xylem Hydrovar HVL
+variable speed drive (VSD) controller.
 
+This module supports just one slave device on the Modbus, not multiple. With
+just one slave device the communication handling gets way simpler as we don't
+have to figure out which reply message belongs to which slave device in case we
+would have multiple slave devices. Also, the query and reply parsing can now be
+handled inside of a single function, instead of having to handle this
+asynchronously across multiple functions.
+
+Reference documents:
+(1) Hydrovar HVL 2.015 - 4.220 Modbus Protocol & Parameters
+------------------------------------------------------------------------
 Function codes:
     0x03: READ COMMAND, read holding registers
     0x06: WRITE COMMAND, write single register
     0x10: WRITE COMMAND, write multiple contiguous registers
 """
+__author__ = "Dennis van Gils"
+__authoremail__ = "vangils.dennis@gmail.com"
+__url__ = "https://github.com/Dennis-van-Gils/python-dvg-devices"
+__date__ = "15-02-2023"
+__version__ = "1.0.0"
 
 import os
+from typing import Tuple, Union
+from pathlib import Path
 from enum import Enum, IntEnum
 
 from dvg_devices.BaseDevice import SerialDevice
+from CRC_check import CRC_check
 
 
-# Xylem Hydrovar HVL variable speed drive (VSD)
-class XylemHydrovarHVL(SerialDevice):
-    class State:
-        """Container for the process and measurement variables."""
+def pretty_format_hex(byte_msg: bytes) -> str:
+    msg = ""
+    for byte in byte_msg:
+        msg += f"{byte:02x} "
+    return msg
 
-        pass
 
-    def __init__(
-        self,
-        name: str = "HVL",
-        long_name: str = "Xylem Hydrovar HVL variable speed drive",
-        path_config: str = (os.getcwd() + "/config/settings_Hydrovar.txt"),
-    ):
-        super().__init__(name=name, long_name=long_name)
+# ------------------------------------------------------------------------------
+#   Enumerations
+# ------------------------------------------------------------------------------
 
 
 class HVL_FuncCode(Enum):
+    # Implemented:
     READ = 0x03  # Read the binary contents of registers
     WRITE = 0x06  # Write a value into a single register
+
+    # Not implemented:
     WRITE_CONT = 0x10  # Write values into a block of contiguous registers
 
 
@@ -49,28 +65,185 @@ class HVL_DType(IntEnum):
     B2 = 7  # 32 bits bitmap
 
 
+# ------------------------------------------------------------------------------
+#   HVL_Register
+# ------------------------------------------------------------------------------
+
+
 class HVL_Register:
-    def __init__(self, modbus_address: int, datum_type: HVL_DType):
-        self.modbus_address = modbus_address
+    def __init__(
+        self,
+        address: int,
+        datum_type: HVL_DType,
+        menu_index: str = "",
+    ):
+        self.address = address  # Modbus address as taken from Table 5, ref (1)
         self.datum_type = datum_type
+        self.menu_index = menu_index
 
 
-# List of registers and corresponding data type
+# ------------------------------------------------------------------------------
+#   XylemHydrovarHVL
+# ------------------------------------------------------------------------------
+
+
+class XylemHydrovarHVL(SerialDevice):
+    class State:
+        """Container for the process and measurement variables."""
+
+        pass
+
+    def __init__(
+        self,
+        name: str = "HVL",
+        long_name: str = "Xylem Hydrovar HVL variable speed drive",
+        # path_config: str = (os.getcwd() + "/config/settings_Hydrovar.txt"),
+        connect_to_modbus_slave_address: int = 0x01,
+    ):
+        super().__init__(name=name, long_name=long_name)
+
+        # Default for RTU is 9600-8N1
+        self.serial_settings = {
+            "baudrate": 9600,
+            "bytesize": 8,
+            "parity": "N",
+            "stopbits": 1,
+            "timeout": 0.2,
+            "write_timeout": 0.2,
+        }
+        self.set_read_termination("")
+        self.set_write_termination("")
+
+        self.set_ID_validation_query(
+            ID_validation_query=self.ID_validation_query,
+            valid_ID_broad=True,
+            valid_ID_specific=connect_to_modbus_slave_address,
+        )
+
+        # Container for the process and measurement variables
+        self.state = self.State()
+
+        # Modbus slave address of the device (P1205)
+        self.modbus_slave_address = connect_to_modbus_slave_address
+
+        # Location of the configuration file
+        # self.path_config = Path(path_config)
+
+    # --------------------------------------------------------------------------
+    #   ID_validation_query
+    # --------------------------------------------------------------------------
+
+    def ID_validation_query(self) -> Tuple[bool, Union[int, None]]:
+        success = self.query_modbus_slave_address()
+        return success, self.modbus_slave_address
+
+    def construct_RTU_read_command(self, register: HVL_Register) -> bytes:
+        """Construct a full byte array including CRC check to send over to the
+        Hydrovar controller"""
+        # fmt: off
+        byte_msg = bytearray(8)
+        byte_msg[0] = self.modbus_slave_address
+        byte_msg[1] = HVL_FuncCode.READ.value
+        byte_msg[2] = (register.address & 0xFF00) >> 8  # address HI
+        byte_msg[3] = register.address & 0x00FF         # address LO
+        byte_msg[4] = 0x00                              # no. of points HI
+        byte_msg[5] = 0x01                              # no. of points LO
+        byte_msg[6:] = CRC_check(byte_msg[:6])
+        # fmt: on
+        print(pretty_format_hex(byte_msg))
+        return byte_msg
+
+    def construct_RTU_write_command(
+        self, register: HVL_Register, value: int
+    ) -> bytes:
+        """Construct a full byte array including CRC check to send over to the
+        Hydrovar controller"""
+        # fmt: off
+        byte_msg = bytearray(8)
+        byte_msg[0] = self.modbus_slave_address
+        byte_msg[1] = HVL_FuncCode.WRITE.value
+        byte_msg[2] = (register.address & 0xFF00) >> 8  # address HI
+        byte_msg[3] = register.address & 0x00FF         # address LO
+        byte_msg[4] = (value & 0xFF00) >> 8             # data HI
+        byte_msg[5] = value & 0x00FF                    # data LO
+        byte_msg[6:] = CRC_check(byte_msg[:6])
+        # fmt: on
+        print(pretty_format_hex(byte_msg))
+        return byte_msg
+
+    # --------------------------------------------------------------------------
+    #
+    # --------------------------------------------------------------------------
+
+    def turn_on_pump(self) -> bool:
+        byte_msg = self.construct_RTU_write_command(HVL_STOP_START, 1)
+        success, reply = self.query(byte_msg, returns_ascii=False)
+
+        # TODO: make more intelligent
+        if isinstance(reply, bytes):
+            print(pretty_format_hex(reply))
+            print(f"DEC VALUE: {int(reply[4:6].hex(), 16):d}")
+
+        return success
+
+    def turn_off_pump(self) -> bool:
+        byte_msg = self.construct_RTU_write_command(HVL_STOP_START, 0)
+        success, reply = self.query(byte_msg, returns_ascii=False)
+
+        # TODO: make more intelligent
+        if isinstance(reply, bytes):
+            print(pretty_format_hex(reply))
+            print(f"DEC VALUE: {int(reply[4:6].hex(), 16):d}")
+
+        return success
+
+    def query_actual_value(self) -> bool:
+        byte_msg = self.construct_RTU_read_command(HVL_ACTUAL_VALUE)
+        success, reply = self.query(byte_msg, returns_ascii=False)
+
+        # TODO: make more intelligent
+        if isinstance(reply, bytes):
+            print(pretty_format_hex(reply))
+            print(f"DEC VALUE: {int(reply[3:5].hex(), 16):d}")
+
+        return success
+
+    # --------------------------------------------------------------------------
+    #   query_modbus_slave_address
+    # --------------------------------------------------------------------------
+
+    def query_modbus_slave_address(self) -> bool:
+        """Query the Modbus slave address of the device (P1205).
+
+        Returns: True if successful, False otherwise.
+        """
+        byte_msg = self.construct_RTU_read_command(HVL_ADDRESS)
+        success, reply = self.query(byte_msg, returns_ascii=False)
+
+        # TODO: make more intelligent
+        if isinstance(reply, bytes):
+            print(pretty_format_hex(reply))
+
+        return success
+
+
+# List of registers (incomplete list, just the bare essentials)
 # fmt: off
-HVL_STOP_START    = HVL_Register(0x31, HVL_DType.U08)
-HVL_ACTUAL_VALUE  = HVL_Register(0x32, HVL_DType.S16)
-HVL_OUTPUT_FREQ   = HVL_Register(0x33, HVL_DType.S16)
-HVL_EFF_REQ_VAL   = HVL_Register(0x37, HVL_DType.U16)
-HVL_START_VALUE   = HVL_Register(0x38, HVL_DType.U08)
-HVL_ENABLE_DEVICE = HVL_Register(0x61, HVL_DType.U08)
+HVL_STOP_START    = HVL_Register(0x0031, HVL_DType.U08, "")
+HVL_ACTUAL_VALUE  = HVL_Register(0x0032, HVL_DType.S16, "")
+HVL_OUTPUT_FREQ   = HVL_Register(0x0033, HVL_DType.S16, "P46")
+HVL_EFF_REQ_VAL   = HVL_Register(0x0037, HVL_DType.U16, "P03")
+HVL_START_VALUE   = HVL_Register(0x0038, HVL_DType.U08, "P04")
+HVL_ENABLE_DEVICE = HVL_Register(0x0061, HVL_DType.U08, "P24")
+HVL_ADDRESS       = HVL_Register(0x010d, HVL_DType.U08, "P1205")
 
 # Diagnostics
-HVL_TEMP_INVERTER = HVL_Register(0x85, HVL_DType.S08)
-HVL_CURR_INVERTER = HVL_Register(0x87, HVL_DType.U16)
-HVL_VOLT_INVERTER = HVL_Register(0x88, HVL_DType.U16)
-HVL_ERROR_RESET   = HVL_Register(0xd3, HVL_DType.U08)
+HVL_TEMP_INVERTER = HVL_Register(0x0085, HVL_DType.S08, "P43")
+HVL_CURR_INVERTER = HVL_Register(0x0087, HVL_DType.U16, "P44")
+HVL_VOLT_INVERTER = HVL_Register(0x0088, HVL_DType.U16, "P45")
+HVL_ERROR_RESET   = HVL_Register(0x00d3, HVL_DType.U08, "P615")
 
 # Special status bits
-HVL_ERRORS_H3     = HVL_Register(0x012d, HVL_DType.B2)
-HVL_DEV_STATUS_H4 = HVL_Register(0x01c1, HVL_DType.B1)
+HVL_ERRORS_H3     = HVL_Register(0x012d, HVL_DType.B2 , "")
+HVL_DEV_STATUS_H4 = HVL_Register(0x01c1, HVL_DType.B1 , "")
 # fmt: on
--- src_python/rs485_pump_test/pump_rs485_rtu_test.py
+++ src_python/rs485_pump_test/pump_rs485_rtu_test.py
@@ -1,85 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import serial
-import time
-
-from CRC_check import CRC_check
-import HVL_Registers as hvl
-
-
-class RTU_single_reg:
-    def __init__(self, slave_addr=0x01, func=0x00, addr_hi=0x00, addr_lo=0x00):
-        self.slave_addr = slave_addr
-        self.func = func
-        self.addr_hi = addr_hi
-        self.addr_lo = addr_lo
-        self.data_hi = 0x00
-        self.data_lo = 0x00
-        self.crc_hi = 0x00
-        self.crc_lo = 0x00
-
-        self.byte_msg = bytearray(8)
-
-    def set_data(self, val):
-        self.data_hi = (val & 0xFF00) >> 8
-        self.data_lo = val & 0x00FF
-
-    def compile(self):
-        self.byte_msg[0] = self.slave_addr
-        self.byte_msg[1] = self.func
-        self.byte_msg[2] = self.addr_hi
-        self.byte_msg[3] = self.addr_lo
-        self.byte_msg[4] = self.data_hi
-        self.byte_msg[5] = self.data_lo
-
-        self.crc_hi, self.crc_lo = CRC_check(self.byte_msg[:6])
-        self.byte_msg[6] = self.crc_hi
-        self.byte_msg[7] = self.crc_lo
-
-        return self.byte_msg
-
-    def __str__(self):
-        msg = ""
-        for byte in self.byte_msg:
-            msg += f"{byte:02x} "
-
-        return msg
-
+from XylemHydrovarHVL_protocol_RTU import XylemHydrovarHVL
 
 if __name__ == "__main__":
-    # Default for RTU is 9600-8N1
-    ser = serial.Serial(
-        port="COM12",
-        baudrate=9600,
-        bytesize=8,
-        parity="N",
-        stopbits=1,
-    )
+    # Path to the textfile containing the (last used) serial port
+    PATH_PORT = "config/port_Hydrovar.txt"
 
-    rtu = RTU_single_reg(func=0x06, addr_hi=0x00, addr_lo=0xE8)
-    rtu.set_data(350)
-    rtu.compile()
-    print(rtu)
+    hvl = XylemHydrovarHVL(connect_to_modbus_slave_address=0x01)
+    hvl.serial_settings = {
+        "baudrate": 9600,
+        "bytesize": 8,
+        "parity": "N",
+        "stopbits": 1,
+        "timeout": 0.2,
+        "write_timeout": 0.2,
+    }
 
-    ser.write(rtu.byte_msg)
-    time.sleep(0.1)
-    ans = ser.read_all()
-
-    if ans is not None:
-        for b in ans:
-            print(f"{b:02x} ", end="")
-    print("")
-
-    # Read baudrate
-    cmd = bytearray([0x01, 0x03, 0x01, 0x0E, 0x00, 0x01])
-    cmd.extend(CRC_check(cmd))
-
-    ser.write(cmd)
-    time.sleep(0.1)
-    ans = ser.read_all()
-
-    if ans is not None:
-        for b in ans:
-            print(f"{b:02x} ", end="")
-            # print(b, end="")
+    if hvl.auto_connect(PATH_PORT):
+        hvl.query_actual_value()
