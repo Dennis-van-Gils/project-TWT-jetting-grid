494b0f81791f0d9ec46ecdc7ece0e6c8babad0d0
Dennis van Gils <37263907+Dennis-van-Gils@users.noreply.github.com>
2023-02-27 17:12:35 +0100

    Create XylemHydrovarHVL_qdev.py

---
+++ src_python/rs485_pump_test/XylemHydrovarHVL_qdev.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""PyQt/Pyside module to provide multithreaded communication and periodical data
+acquisition for a Xylem Hydrovar HVL variable speed drive (VSD) controller.
+"""
+__author__ = "Dennis van Gils"
+__authoremail__ = "vangils.dennis@gmail.com"
+__url__ = "https://github.com/Dennis-van-Gils/python-dvg-devices"
+__date__ = "27-02-2023"
+__version__ = "1.0.0"
+# pylint: disable=invalid-name
+
+import os
+import sys
+import time
+
+# Mechanism to support both PyQt and PySide
+# -----------------------------------------
+
+PYQT5 = "PyQt5"
+PYQT6 = "PyQt6"
+PYSIDE2 = "PySide2"
+PYSIDE6 = "PySide6"
+QT_LIB_ORDER = [PYQT5, PYSIDE2, PYSIDE6, PYQT6]
+QT_LIB = None
+
+if QT_LIB is None:
+    for lib in QT_LIB_ORDER:
+        if lib in sys.modules:
+            QT_LIB = lib
+            break
+
+if QT_LIB is None:
+    for lib in QT_LIB_ORDER:
+        try:
+            __import__(lib)
+            QT_LIB = lib
+            break
+        except ImportError:
+            pass
+
+if QT_LIB is None:
+    this_file = __file__.split(os.sep)[-1]
+    raise ImportError(
+        f"{this_file} requires PyQt5, PyQt6, PySide2 or PySide6; "
+        "none of these packages could be imported."
+    )
+
+# fmt: off
+# pylint: disable=import-error, no-name-in-module, unused-import
+if QT_LIB == PYQT5:
+    from PyQt5 import QtCore, QtGui, QtWidgets as QtWid    # type: ignore
+    from PyQt5.QtCore import pyqtSlot as Slot              # type: ignore
+    from PyQt5.QtCore import pyqtSignal as Signal          # type: ignore
+elif QT_LIB == PYQT6:
+    from PyQt6 import QtCore, QtGui, QtWidgets as QtWid    # type: ignore
+    from PyQt6.QtCore import pyqtSlot as Slot              # type: ignore
+    from PyQt6.QtCore import pyqtSignal as Signal          # type: ignore
+elif QT_LIB == PYSIDE2:
+    from PySide2 import QtCore, QtGui, QtWidgets as QtWid  # type: ignore
+    from PySide2.QtCore import Slot                        # type: ignore
+    from PySide2.QtCore import Signal                      # type: ignore
+elif QT_LIB == PYSIDE6:
+    from PySide6 import QtCore, QtGui, QtWidgets as QtWid  # type: ignore
+    from PySide6.QtCore import Slot                        # type: ignore
+    from PySide6.QtCore import Signal                      # type: ignore
+# pylint: enable=import-error, no-name-in-module, unused-import
+# fmt: on
+
+# \end[Mechanism to support both PyQt and PySide]
+# -----------------------------------------------
+
+from dvg_pyqt_controls import create_Toggle_button
+from dvg_debug_functions import dprint, print_fancy_traceback as pft
+
+from dvg_qdeviceio import QDeviceIO, DAQ_TRIGGER
+from XylemHydrovarHVL_protocol_RTU import XylemHydrovarHVL
+
+
+class XylemHydrovarHVL_qdev(QDeviceIO):
+    """Manages multithreaded communication and periodical data acquisition for
+    a Xylem Hydrovar HVL variable speed drive (VSD) controller.
+
+    In addition, it also provides PyQt/PySide GUI objects for control of the
+    device. These can be incorporated into your application.
+
+    All device I/O operations will be offloaded to 'workers', each running in
+    a newly created thread.
+
+    (*): See 'dvg_qdeviceio.QDeviceIO()' for details.
+
+    Args:
+        dev:
+            Reference to a 'XylemHydrovarHVL_protocol_RTU.XylemHydrovarHVL'
+            instance.
+
+        debug:
+            Show debug info in terminal? Warning: Slow! Do not leave on
+            unintentionally.
+
+    Main GUI objects:
+        qgrp (PyQt5.QtWidgets.QGroupBox)
+    """
+
+    signal_GUI_input_field_update = Signal(int)
+
+    def __init__(
+        self,
+        dev: XylemHydrovarHVL,
+        DAQ_trigger=DAQ_TRIGGER.INTERNAL_TIMER,
+        DAQ_interval_ms=200,
+        DAQ_timer_type=QtCore.Qt.TimerType.CoarseTimer,
+        critical_not_alive_count=3,
+        debug=False,
+        **kwargs,
+    ):
+        super().__init__(dev, **kwargs)  # Pass kwargs onto QtCore.QObject()
+
+        self.create_worker_DAQ(
+            DAQ_trigger=DAQ_trigger,
+            DAQ_function=self.DAQ_function,
+            DAQ_interval_ms=DAQ_interval_ms,
+            DAQ_timer_type=DAQ_timer_type,
+            critical_not_alive_count=critical_not_alive_count,
+            debug=debug,
+        )
+        self.create_worker_jobs(jobs_function=self.jobs_function, debug=debug)
+
+        self.create_GUI()
+        self.signal_DAQ_updated.connect(self.update_GUI)
+        self.signal_connection_lost.connect(self.update_GUI)
+        self.signal_GUI_input_field_update.connect(self.update_GUI_input_field)
+
+        self.update_GUI()
+        self.update_GUI_input_field()
